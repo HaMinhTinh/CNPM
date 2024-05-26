2ca1213765e29b67a8323e1b7070930d489b32e8
HaMinhTinh <20130438@st.hcmuaf.edu.vn>
2024-05-26 20:48:49 +0700

    tạo phương thức write() để ghi chuỗi vào file

--- src/EasyHighScores.java
+++ src/EasyHighScores.java
@@ -10,6 +10,26 @@ public class EasyHighScores {
 
     // Phương thức File() dùng để ghi và đọc
     private void File() {
-
+        try {
+            File file = new File("EasyHighScores.txt");
+            file.setWritable(true);// Đặt quyền ghi cho file
+            file.setReadable(true);// Đặt quyền đọc cho file
+            this.fileWriter = new FileWriter("EasyHighScores.txt", true);
+            this.printWriter = new PrintWriter(this.fileWriter);
+        } catch (IOException var2) {
+            var2.printStackTrace();
+        }
     }
+
+    //Khởi tạo phương thức write() với tham số truyền vào có kiểu dữ liệu là String để ghi chuối vào file
+    public void write(String string) {
+        try {
+            this.printWriter.println(string + "\n");
+            this.printWriter.flush();
+            this.fileWriter.close();
+        } catch (IOException var3) {
+            var3.printStackTrace();
+        }
+    }
+
 }
