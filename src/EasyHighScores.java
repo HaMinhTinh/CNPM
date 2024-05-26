259f91f87687f9be1b19e2d65b080b01728e0524
HaMinhTinh <20130438@st.hcmuaf.edu.vn>
2024-05-26 20:39:28 +0700

    tạo các class EasyHighScores, MediumHighScores, HardHighScores để ghi và đọc tệp điểm số vào Phương thức File()

---
+++ src/EasyHighScores.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class EasyHighScores {
+    private FileWriter fileWriter;
+    private PrintWriter printWriter;
+
+    public EasyHighScores() {
+        File();
+    }
+
+    // Phương thức File() dùng để ghi và đọc
+    private void File() {
+
+    }
+}
